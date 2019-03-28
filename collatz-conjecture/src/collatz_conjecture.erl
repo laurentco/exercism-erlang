9b5e1c2e92560b8b6d72a36fdc80dc1c7374225e
Laurent <laurentco@users.noreply.github.com>
2019-03-29 09:29:14 +1100

    Guarding for correct input rather than the other way around

--- collatz-conjecture/src/collatz_conjecture.erl
+++ collatz-conjecture/src/collatz_conjecture.erl
@@ -2,8 +2,8 @@
 
 -export([steps/1]).
 
-steps(N) when N =< 0              -> {error, "Only positive numbers are allowed"};
-steps(N)                          -> steps(N,0).
+steps(N) when is_integer(N), N>0  -> steps(N, 0);
+steps(_)                          -> {error, "Only positive numbers are allowed"}.
 steps(1, Count)                   -> Count;
 steps(N, Count) when N rem 2 == 0 -> steps(N div 2, Count + 1);
 steps(N, Count)                   -> steps(3 * N + 1, Count + 1).

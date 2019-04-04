e97f8834fa4491523370d2eefdf3efb41666e0c7
Laurent <laurentco@users.noreply.github.com>
2019-04-05 09:25:58 +1100

    moved list init into a constant

--- raindrops/src/raindrops.erl
+++ raindrops/src/raindrops.erl
@@ -1,9 +1,10 @@
 -module(raindrops).
 
+-define(FACTORS, [{3,"Pling"}, {5,"Plang"}, {7, "Plong"}]).
 -export([convert/1]).
 
 convert(Number) ->
-  case [ Output || {Factor, Output} <- [{3,"Pling"}, {5,"Plang"}, {7, "Plong"}], Number rem Factor =:= 0] of
+  case [ Output || {Factor, Output} <- ?FACTORS, Number rem Factor =:= 0] of
     []  -> integer_to_list(Number);
     Str -> lists:flatten(Str)
   end.

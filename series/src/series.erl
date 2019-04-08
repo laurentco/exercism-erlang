39947878e5284b328caf70de4ead58ba705909d0
Laurent <laurentco@users.noreply.github.com>
2019-04-08 16:52:59 +1000

    Save one more length call

--- series/src/series.erl
+++ series/src/series.erl
@@ -2,8 +2,11 @@
 
 -export([slices/2]).
 
-slices(SliceLength, Series = [_|_]) when SliceLength =< length(Series) ->
-  lists:reverse(slices(SliceLength, Series, length(Series) - SliceLength + 1, [])).
+slices(SliceLength, Series = [_|_])  ->
+  slices(SliceLength, Series, length(Series)).
+
+slices(SliceLength, Series, SeriesLength) when SliceLength =< SeriesLength ->
+  lists:reverse(slices(SliceLength, Series, SeriesLength - SliceLength + 1, [])).
 
 slices(_, _, 0, Acc) -> Acc;
 slices(SliceLength, Series = [_|T], Count, Acc) ->

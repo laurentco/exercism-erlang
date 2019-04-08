b7a775983e49073fc1f227e5ac546db285cc4788
Laurent <laurentco@users.noreply.github.com>
2019-04-09 09:29:44 +1000

    moved list:reverse to the base case of the recursion

--- series/src/series.erl
+++ series/src/series.erl
@@ -6,8 +6,8 @@ slices(SliceLength, Series = [_|_])  ->
   slices(SliceLength, Series, length(Series)).
 
 slices(SliceLength, Series, SeriesLength) when SliceLength =< SeriesLength ->
-  lists:reverse(slices(SliceLength, Series, SeriesLength - SliceLength + 1, [])).
+  slices(SliceLength, Series, SeriesLength - SliceLength + 1, []).
 
-slices(_, _, 0, Acc) -> Acc;
+slices(_, _, 0, Acc) -> lists:reverse(Acc);
 slices(SliceLength, Series = [_|T], Count, Acc) ->
   slices(SliceLength, T, Count - 1, [lists:sublist(Series,SliceLength)|Acc]).

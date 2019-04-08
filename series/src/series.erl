d3518ba2d3fd050beedf3cfc0455bedbe94f6d52
Laurent <laurentco@users.noreply.github.com>
2019-04-08 16:41:46 +1000

    much improved Series exercise

--- series/src/series.erl
+++ series/src/series.erl
@@ -2,16 +2,9 @@
 
 -export([slices/2]).
 
-slices(SliceLength, Series = [_|_]) when SliceLength =< length(Series), SliceLength > 0 ->
-  lists:reverse(slices(SliceLength, Series, [])).
+slices(SliceLength, Series = [_|_]) when SliceLength =< length(Series) ->
+  lists:reverse(slices(SliceLength, Series, length(Series) - SliceLength + 1, [])).
 
-slices(_, [],Acc) -> Acc;
-slices(SliceLength, Series = [_|T], Acc) ->
-  case takeN(SliceLength, Series) of
-    Slice when length(Slice) == SliceLength -> slices(SliceLength, T, [Slice|Acc]);
-                                          _ -> Acc
-  end.
-
-takeN(N, List) -> lists:reverse(takeN(N, List, [])).
-takeN(N, [Take|List], Taken) when N > 0 -> takeN(N-1, List, [Take|Taken]);
-takeN(_, _, Taken) -> Taken.
+slices(_, _, 0, Acc) -> Acc;
+slices(SliceLength, Series = [_|T], Count, Acc) ->
+  slices(SliceLength, T, Count - 1, [lists:sublist(Series,SliceLength)|Acc]).

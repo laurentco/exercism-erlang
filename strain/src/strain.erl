e6bfa44c96051ac0100638b6faaa1cde0d8ee585
Laurent <laurentco@users.noreply.github.com>
2019-05-03 10:45:24 +1000

    Refactored the strain solution to not use lists:filter

--- strain/src/strain.erl
+++ strain/src/strain.erl
@@ -2,8 +2,16 @@
 
 -export([keep/2, discard/2]).
 
-keep(Fn, List) ->
-  lists:filter(Fn, List).
+keep(Fn, List) -> filter(Fn, List).
 
-discard(Fn, List) ->
-  lists:filter(fun(E) -> not(Fn(E)) end, List).
+discard(Fn, List) -> keep(fun(E) -> not(Fn(E)) end, List).
+
+filter(Fn, List) -> lists:reverse(filter(Fn, List, [])).
+
+filter(_, [], List) -> List;
+filter(Fn, [Element | List], Filtered) ->
+  case Fn(Element) of
+    true  -> filter(Fn, List, [Element | Filtered]);
+    false -> filter(Fn, List, Filtered)
+  end.
+

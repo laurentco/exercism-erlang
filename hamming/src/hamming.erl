aee95813da35ab7e610132fc70065176f3648b07
Laurent <laurentco@users.noreply.github.com>
2019-05-06 13:29:57 +1000

    initial solution to Hamming distance exercise

---
+++ hamming/src/hamming.erl
@@ -0,0 +1,12 @@
+-module(hamming).
+
+-export([distance/2]).
+-define(LENGTH_ERROR, {error, "left and right strands must be of equal length"}).
+
+distance(Strand1, Strand2) -> distance(Strand1, Strand2, 0).
+
+distance([], [], Distance) -> Distance;
+distance([], _, _) -> ?LENGTH_ERROR;
+distance(_, [], _) -> ?LENGTH_ERROR;
+distance([E|Strand1], [E|Strand2], Distance) -> distance(Strand1, Strand2, Distance);
+distance([_|Strand1], [_|Strand2], Distance) -> distance(Strand1, Strand2, Distance + 1).

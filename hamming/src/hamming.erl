db55212e3b3e915b73aa0ee93576891ea26d2bda
Laurent <laurentco@users.noreply.github.com>
2019-05-06 15:11:21 +1000

    Revision optimised for the equal-length long strand use case

--- hamming/src/hamming.erl
+++ hamming/src/hamming.erl
@@ -5,8 +5,8 @@
 
 distance(Strand1, Strand2) -> distance(Strand1, Strand2, 0).
 
+distance([E|Strand1], [E|Strand2], Distance) -> distance(Strand1, Strand2, Distance);
+distance([_|Strand1], [_|Strand2], Distance) -> distance(Strand1, Strand2, Distance + 1);
 distance([], [], Distance) -> Distance;
 distance([], _, _) -> ?LENGTH_ERROR;
-distance(_, [], _) -> ?LENGTH_ERROR;
-distance([E|Strand1], [E|Strand2], Distance) -> distance(Strand1, Strand2, Distance);
-distance([_|Strand1], [_|Strand2], Distance) -> distance(Strand1, Strand2, Distance + 1).
+distance(_, [], _) -> ?LENGTH_ERROR.
